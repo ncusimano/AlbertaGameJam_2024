ebb37edb461e2df007b700eba7e2284f65ed33b8
ncusimano <cusimano@ualberta.ca>
2024-08-10 19:52:24 -0600

    Keypad Updates

--- GameFiles/Assets/Scripts/Button.gd
+++ GameFiles/Assets/Scripts/Button.gd
@@ -1,14 +1,15 @@
 extends Button
 
-@export var buttonnum = 0
+@export var button_num = 0
 @export var animation = ""
 
-signal custombuttonpressed(number)
+signal custom_button_pressed(number)
+signal custom_button_released(number)
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$AnimatedSprite2D.set_animation(animation)
-
+	#$AnimatedSprite2D.set_animation(animation)
+	set_action_mode(ACTION_MODE_BUTTON_PRESS)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -16,10 +17,8 @@ func _ready():
 	# pass
 
 
-func _on_pressed():
-	var number = buttonnum
-	custombuttonpressed.emit(number)
+func _pressed():
+	custom_button_pressed.emit(button_num)
 	# Play the pressing animation.
-	$AnimatedSprite2D.play()
-
+	#$AnimatedSprite2D.play()
 
---
+++ GameFiles/Assets/Scripts/kypd.gd
@@ -0,0 +1,38 @@
+extends Node2D
+var timer = null
+var current_button = null
+var timer_cycles = 0
+var time_pressed = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	# Connect and configure the timer for tracking button press length.
+	timer = get_node("Timer")
+	timer.timeout.connect(_on_timer_timeout)
+	timer.set_one_shot(false)
+
+	# Connect button signals for each button.
+	for child in (get_children()):
+		if (has_signal("custom_button_pressed")):
+			child.custom_button_pressed.connect(_on_button_pressed)
+			child.button_up.connect(_on_button_released)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta):
+	if (!timer.is_stopped()):
+		time_pressed = timer_cycles * timer.wait_time + (timer.wait_time - timer.time_left)
+
+
+func _on_button_pressed(number):
+	current_button = number
+	timer_cycles = 0
+	time_pressed = 0
+	timer.start()
+
+func _on_button_released():
+	timer.stop()
+	current_button = null
+
+func _on_timer_timeout():
+	timer_cycles += 1
